130ba4b18dabb376882b2c9f277feb7acc3894d2
Kurt Stephens <kurt@enova.com>
2012-11-09 21:33:59 -0600

    More ruby 1.8 support.

--- lib/pointy_hair.rb
+++ lib/pointy_hair.rb
@@ -4,6 +4,9 @@ require "pointy_hair/version"
 module PointyHair
 end
 
+if RUBY_VERSION =~ /^1\.8/
+  require 'pointy_hair/ruby_18'
+end
 require 'pointy_hair/work'
 require 'pointy_hair/worker'
 require 'pointy_hair/manager'
