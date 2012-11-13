c14b6ec921e434d7a859e9f0dfdcb6fa0827a3dc
Kurt Stephens <kurt@enova.com>
2012-11-13 13:33:55 -0600

    Scaffold put_work_back!.

--- lib/pointy_hair/worker/test.rb
+++ lib/pointy_hair/worker/test.rb
@@ -9,7 +9,11 @@ module PointyHair
 
       def get_work!
         sleep(1 + rand)
-        @counter += 1
+        if file_exists?(old_work_file)
+          @counter = get_old_work! || 0
+        else
+          @counter += 1
+        end
       end
 
       def work! work
@@ -18,6 +22,26 @@ module PointyHair
         write_file! "output" do | fh |
           fh.puts work.to_s
         end
+      end
+
+      def put_work_back! work
+        save_old_work! work
+      end
+
+      def save_old_work! work
+        write_file!(old_work_file) do | fh |
+          fh.write YAML.dump(work)
+        end
+      end
+
+      def get_old_work!
+        read_file!(old_work_file) do | fh |
+          YAML.load(fh)
+        end
+      end
+
+      def old_work_file
+        '../old_work'
       end
     end
   end
