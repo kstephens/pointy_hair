11e67d373d234bf2c0d51c8981260d544e700282
Kurt Stephens <kurt@enova.com>
2012-11-13 18:35:37 -0600

    Mock out Manager#get_work!.

--- spec/manager_spec.rb
+++ spec/manager_spec.rb
@@ -4,6 +4,18 @@ require 'pointy_hair'
 require 'pointy_hair/worker/test'
 
 describe PointyHair::Manager do
+  attr_accessor :m, :base_dir, :exit_code
+
+  before(:each) do
+    self.base_dir = File.expand_path("../../tmp/spec/#{$$}", __FILE__)
+    self.m = PointyHair::Manager.new
+    m.base_dir = base_dir
+  end
+
+  after(:each) do
+    FileUtils.rm_rf(base_dir)
+  end
+
   it "should start up N workers" do
     mgr = PointyHair::Manager.new
     mgr.worker_config = {
@@ -17,5 +29,15 @@ describe PointyHair::Manager do
         }
       }
     }
+    def mgr.get_work!
+      $stderr.puts "working #{@work_id}"
+      if @work_id > 5
+        stop!
+      else
+        pp get_child_pids
+      end
+      super
+    end
+    mgr.run!
   end
 end
