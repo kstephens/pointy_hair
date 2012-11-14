de97d8d25288a62aeb76222b414d69fc5b1fcb90
Kurt Stephens <kurt@enova.com>
2012-11-14 02:59:20 -0600

    Added encoding : utf-8.

--- lib/pointy_hair/pause_support.rb
+++ lib/pointy_hair/pause_support.rb
@@ -1,3 +1,5 @@
+# -- encoding : utf-8 --
+
 module PointyHair
   module PauseSupport
     attr_accessor :pause_interval
--- lib/pointy_hair/state_support.rb
+++ lib/pointy_hair/state_support.rb
@@ -1,3 +1,4 @@
+# -- encoding : utf-8 --
 require 'socket' # gethostname
 require 'fileutils'
 require 'yaml'
